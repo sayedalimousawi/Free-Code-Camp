e7a5fcfdfc3bc5c7c3719072f0313cb9cc6e9965
sayedalimousawi <sayedalimousawi@gmail.com>
2022-11-08 21:01:01 +0330

    Created

--- Basic Algorithm Scripting/Mutations.js
+++ Basic Algorithm Scripting/Mutations.js
@@ -5,8 +5,19 @@
 // Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
 
 function mutation(arr) {
-    
-  return arr;
+  let arr1 = arr[0].toLowerCase().split("");
+  let arr2 = arr[1].toLowerCase().split("");
+  let result = [];
+  for (let i = 0; i < arr2.length; i++) {
+    const myletter = arr2[i];
+    if (arr1.includes(myletter) !== true) {
+      result.push(myletter);
+    }
+  }
+  if (result.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
 console.log(mutation(["hello", "hey"]));
